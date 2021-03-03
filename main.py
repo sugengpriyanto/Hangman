0d51596694f9c5989a315e19d8831f69bd1555e6
Sugeng Priyanto <spriyanto360@gmail.com>
2021-03-03 09:00:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,103 @@
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#check player's life
+lives = 6
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #if letter guessed more than once
+    if guess in display:
+      print(f"You have already guessed {guess}")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+     
+    #reducing life
+    if guess not in chosen_word:
+      print("You guessed a wrong letter. You lose a life.")
+      lives -= 1
+      if lives  == 0:
+        end_of_game = True
+        print("You lose")
+    #turn into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if all letters answered.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #ASCII Graphic
+    print(stages[lives])
